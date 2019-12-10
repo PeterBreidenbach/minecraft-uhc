a04c60b2f6b56f39bd2812cc2c2a29e9d725eb24
Peter <peter-maximilian.breidenbach@adesso.de>
2019-12-10 23:20:55 +0100

    Changed alivePlayers to HashSet

--- src/de/breidenbach/uhc/Game.java
+++ src/de/breidenbach/uhc/Game.java
@@ -5,10 +5,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Game {
@@ -17,7 +14,7 @@ public class Game {
     public boolean started;
     public boolean invulnerable;
     public boolean finished;
-    public List<Player> alivePlayers;
+    public HashSet<Player> alivePlayers;
     private JavaPlugin plugin;
     private int countDownTimerAddress;
     private int matchTimerAddress;
@@ -26,10 +23,10 @@ public class Game {
 
     public Game(JavaPlugin plugin) {
         this.plugin = plugin;
-        this.alivePlayers = new ArrayList<>();
+        this.alivePlayers = new HashSet<>();
     }
 
-    public void init(){
+    public void init() {
         World overworld = plugin.getServer().getWorlds().get(0);
         fillSpawn(overworld, Material.GLASS);
         overworld.getWorldBorder().setCenter(0, 0);
@@ -41,11 +38,11 @@ public class Game {
         active = true;
     }
 
-    public void start(){
+    public void start() {
         countdownStarted = true;
         countDownTimerValue = 60;
         countDownTimerAddress = plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, () -> {
-            switch (countDownTimerValue){
+            switch (countDownTimerValue) {
                 case 60:
                 case 30:
                 case 10:
@@ -65,41 +62,39 @@ public class Game {
         }, 0, 20);
     }
 
-    public void handlePlayerJoin(Player p){
-        p.teleport(new Location(p.getWorld(), 0, p.getWorld().getHighestBlockYAt(0,0),0));
-        if(!started) {
+    public void handlePlayerJoin(Player p) {
+        p.teleport(new Location(p.getWorld(), 0, p.getWorld().getHighestBlockYAt(0, 0), 0));
+        if (!started) {
             alivePlayers.add(p);
             p.setFoodLevel(20);
             p.setGameMode(GameMode.ADVENTURE);
-        }else{
+        } else {
             p.setGameMode(GameMode.SPECTATOR);
         }
     }
 
-    public void handlePlayerLeave(Player p){
-        if(countdownStarted){
+    public void handlePlayerLeave(Player p) {
+        if (countdownStarted) {
             alivePlayers.remove(p);
-            if(alivePlayers.size() < 2) {
+            if (alivePlayers.size() < 2) {
                 plugin.getServer().getScheduler().cancelTask(countDownTimerAddress);
                 plugin.getServer().broadcastMessage(ChatColor.RED + "Start abort, not enough players!");
                 countdownStarted = false;
             }
-            }else {
-            if (started) {
-                killPlayer(p, "leaving");
-            }
+        } else {
+            killPlayer(p, "leaving");
         }
     }
 
-    private void startMatch(){
-        matchTimerAddress = plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, ()->{
-            switch(matchTimerValue){
+    private void startMatch() {
+        matchTimerAddress = plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, () -> {
+            switch (matchTimerValue) {
                 case 0:
                     fillSpawn(plugin.getServer().getWorlds().get(0), Material.AIR);
                     alivePlayers.forEach(p -> p.playSound(p.getLocation(), Sound.NOTE_PLING, 2.0f, 2.0f));
                     alivePlayers.forEach(p -> p.setGameMode(GameMode.SURVIVAL));
                     plugin.getServer().broadcastMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + "Match started!" + ChatColor.RESET + " You are invulnerable for one minute. PVP will activate in 15 minutes!");
-                    plugin.getServer().getWorlds().get(0).getWorldBorder().setSize((300+Math.log(alivePlayers.size()-1)*300), 10);
+                    plugin.getServer().getWorlds().get(0).getWorldBorder().setSize((300 + Math.log(alivePlayers.size() - 1) * 300), 10);
                     plugin.getServer().getScheduler().cancelTask(countDownTimerAddress);
                     countdownStarted = false;
                     started = true;
@@ -111,34 +106,34 @@ public class Game {
                     break;
                 case 60:
                 case 90:
-                case 2*60:
+                case 2 * 60:
                     //SHOW MESSAGE 10/5/1 MINUTE(S) LEFT
-                    plugin.getServer().broadcastMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + (15 - matchTimerValue/60) + ChatColor.RESET + " minute" + ((15 - matchTimerValue/60) == 1 ? "" : "s") + " until PVP is enabled!");
+                    plugin.getServer().broadcastMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + (15 - matchTimerValue / 60) + ChatColor.RESET + " minute" + ((15 - matchTimerValue / 60) == 1 ? "" : "s") + " until PVP is enabled!");
                     break;
-                case 2*60+30:
+                case 2 * 60 + 30:
                     //ENABLE PVP
                     plugin.getServer().getWorlds().get(0).setPVP(true);
                     plugin.getServer().broadcastMessage(ChatColor.RED + "" + ChatColor.BOLD + "PVP is now enabled!");
                     break;
-                case 3*60:
-                case 3*60+20:
-                case 3*60+40:
+                case 3 * 60:
+                case 3 * 60 + 20:
+                case 3 * 60 + 40:
                     //SHOW BORDER TIMER
-                    plugin.getServer().broadcastMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + (30 - matchTimerValue/60) + ChatColor.RESET + " minute" + ((30 - matchTimerValue/60) == 1 ? "" : "s") + " until the border starts shrinking!");
+                    plugin.getServer().broadcastMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + (30 - matchTimerValue / 60) + ChatColor.RESET + " minute" + ((30 - matchTimerValue / 60) == 1 ? "" : "s") + " until the border starts shrinking!");
                     break;
-                case 4*60:
+                case 4 * 60:
                     // START BORDER SHRINKING
                     plugin.getServer().broadcastMessage(ChatColor.RED + "" + ChatColor.BOLD + "The border will now start shrinking!");
-                    plugin.getServer().getWorlds().get(0).getWorldBorder().setSize(50, 30*60);
+                    plugin.getServer().getWorlds().get(0).getWorldBorder().setSize(50, 30 * 60);
                     break;
             }
             matchTimerValue++;
         }, 0, 20);
     }
 
-    private void checkForWinner(){
-        if(alivePlayers.size() == 1){
-            Player winner = alivePlayers.get(0);
+    private void checkForWinner() {
+        if (alivePlayers.size() == 1) {
+            Player winner = alivePlayers.toArray(new Player[0])[0];
             winner.sendMessage(ChatColor.BOLD + "" + ChatColor.GOLD + "You won!");
             winner.getWorld().getPlayers().stream().filter(i -> i != winner).forEach(p -> {
                 p.sendMessage(ChatColor.BOLD + "" + ChatColor.GOLD + winner.getName() + " won!");
@@ -147,11 +142,11 @@ public class Game {
             plugin.getServer().getScheduler().cancelTask(matchTimerAddress);
             finished = true;
             plugin.getServer().broadcastMessage(ChatColor.BOLD + "The Server will shutdown in 30 seconds!");
-            plugin.getServer().getScheduler().runTaskLaterAsynchronously(plugin, () -> plugin.getServer().shutdown(),600);
+            plugin.getServer().getScheduler().runTaskLaterAsynchronously(plugin, () -> plugin.getServer().shutdown(), 600);
         }
     }
 
-    public void killPlayer(Player p, String damageCause){
+    public void killPlayer(Player p, String damageCause) {
         p.getWorld().strikeLightningEffect(p.getLocation());
         Arrays.stream(p.getInventory().getContents()).filter(s -> Objects.nonNull(s) && s.getType() != Material.AIR).forEach(x -> p.getWorld().dropItemNaturally(p.getLocation(), x));
         Arrays.stream(p.getInventory().getArmorContents()).filter(s -> Objects.nonNull(s) && s.getType() != Material.AIR).forEach(x -> p.getWorld().dropItemNaturally(p.getLocation(), x));
@@ -161,14 +156,14 @@ public class Game {
         p.setHealth(p.getMaxHealth());
         p.getServer().getOnlinePlayers().stream().filter(i -> i != p).forEach(x -> x.sendMessage(ChatColor.RED + p.getName() + " died!"));
         p.sendMessage(ChatColor.RED + "You where killed by " + damageCause + "!");
-        if(alivePlayers.remove(p)){
+        if (alivePlayers.remove(p)) {
             checkForWinner();
         }
     }
 
-    private void fillSpawn(World w, Material m){
-        for(int i = -5; i <= 5; i++){
-            for(int j = -5; j <= 5; j++){
+    private void fillSpawn(World w, Material m) {
+        for (int i = -5; i <= 5; i++) {
+            for (int j = -5; j <= 5; j++) {
                 new Location(w, i, 200, j).getBlock().setType(m);
             }
         }
